2410644086615c661222d7ac0796c2c10cac3115
ByakkoSamma <byakko-ax@yandex.com>
2021-03-12 22:42:36 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,7 @@
+<?php
+$BOT_TOKEN = "1612205654:AAGg_E42alKxw672teF7ls4CH9zNdnwxExA";
 
+$url = 'https://api.telegram.org/bot$BOT_TOKEN/'
+$update = file_get_contents('https://api.telegram.org/bot');
+$update = json_decode($update, true);
+$userChatId = $update["message"]["from"]["id"]?$update["message"]["from"]["id"]:null;
