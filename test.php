d8227cb71a47be59f46728f0a20aad1b5d001360
ByakkoSamma <byakko-ax@yandex.com>
2021-03-12 23:20:39 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,18 @@
 <?php
 $BOT_TOKEN = "1612205654:AAGg_E42alKxw672teF7ls4CH9zNdnwxExA";
 
-$url = "https://api.telegram.org/bot" . $BOT_TOKEN
+$website = "https://api.telegram.org/bot" . $BOT_TOKEN
 $update = file_get_contents('php://input');
 $update = json_decode($update, true);
-$userChatId = $update["message"]["from"]["id"];
 
+$chat_id = $update["message"]["from"]["id"];
+$message = $update["message"]["text"];
+
+if ( $message == "/help" ) {
+  SendMessage($chat_id, "Bobo o qué")
+}
+
+function SendMessage($chat_id, $message) {
+  $url = $GLOBAS[website] . "/sendMessage?chat_id=" . $chat_id . "&parse_mode=HTML&text=" . urlencode($message)';
+  file_get_contents( $url );
+}
